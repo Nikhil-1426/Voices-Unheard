8b04900694427c3fbd266c4001513b1c7547c782
Aditi07B <aditibambal07@gmail.com>
2025-02-23 03:58:39 +0530

    Added License

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 [Your Name or Team Name]
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:share_plus/share_plus.dart';
