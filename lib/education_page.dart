c8b65c6e4c9fcc3f99c374e973333565e964fcdd
Aditi07B <154426963+Aditi07B@users.noreply.github.com>
2025-03-01 02:29:31 +0530

    added license header in  education_page.dart

--- lib/education_page.dart
+++ lib/education_page.dart
@@ -1,3 +1,6 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at
+// https://mozilla.org/MPL/2.0/.
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'community_page.dart';
